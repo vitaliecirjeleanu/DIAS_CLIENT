9391dd9e395af63816db1e19531c08dc25fa0863
Vitalie <vitalie.cirjeleanu@gmail.com>
2024-05-03 16:35:37 +0300

    create topic-details component sample and integrate it with the router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { OverviewComponent } from './components/overview/overview/overview.component';
+import { TopicDetailsComponent } from './components/topic-details/topic-details/topic-details.component';
 
 const routes: Routes = [
   {
     path: 'overview',
     component: OverviewComponent,
+  },
+  {
+    path: 'topic/:name',
+    component: TopicDetailsComponent,
   },
   {
     path: '',
---
+++ src/app/components/topic-details/topic-details/topic-details.component.ts
@@ -0,0 +1,14 @@
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-topic-details',
+  standalone: true,
+  templateUrl: './topic-details.component.html',
+  styleUrl: './topic-details.component.scss',
+})
+export class TopicDetailsComponent {
+  private readonly route = inject(ActivatedRoute);
+
+  public topicName = this.route.snapshot.paramMap.get('name')?.toUpperCase();
+}
