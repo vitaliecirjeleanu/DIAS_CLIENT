d7c98f94afc24e53d0fc7439a3dd7e7959597e8e
Vitalie <vitalie.cirjeleanu@gmail.com>
2024-05-06 19:27:26 +0300

    Revert "feat:(core) - Add api related endpoints to .env file [int_football endpoints]"

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,14 +2,6 @@ interface ImportMeta {
   readonly env: ImportMetaEnv;
 }
 
-interface ImportMetaEnv extends APIEndpoint {
+interface ImportMetaEnv {
   readonly NODE_ENV: string;
-}
-
-interface APIEndpoint {
-  readonly NG_APP_API_BASE_URL: string;
-  readonly NG_APP_API_ALL_TOPICS: string;
-  readonly NG_APP_API_INT_FOOTBALL_GOALSCORERS: string;
-  readonly NG_APP_API_INT_FOOTBALL_RESULTS: string;
-  readonly NG_APP_API_INT_FOOTBALL_SHOOTOUTS: string;
 }
