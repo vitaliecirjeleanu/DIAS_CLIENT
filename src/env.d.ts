2f1fcd20ea7c4233cd1f68cb40621dedcbffb883
Vitalie <vitalie.cirjeleanu@gmail.com>
2024-05-06 19:31:06 +0300

    feat:(core) - Add api related endpoints to .env file

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,6 +2,14 @@ interface ImportMeta {
   readonly env: ImportMetaEnv;
 }
 
-interface ImportMetaEnv {
+interface ImportMetaEnv extends APIEndpoint {
   readonly NODE_ENV: string;
+}
+
+interface APIEndpoint {
+  readonly NG_APP_API_BASE_URL: string;
+  readonly NG_APP_API_ALL_TOPICS: string;
+  readonly NG_APP_API_INT_FOOTBALL_GOALSCORERS: string;
+  readonly NG_APP_API_INT_FOOTBALL_RESULTS: string;
+  readonly NG_APP_API_INT_FOOTBALL_SHOOTOUTS: string;
 }
