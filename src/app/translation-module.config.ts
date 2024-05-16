dd02d41754add2a0638947946e7fe00b0cd74cc1
Vitalie <vitalie.cirjeleanu@gmail.com>
2024-05-16 15:13:43 +0300

    Install needed libraries and create initial config

---
+++ src/app/translation-module.config.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
+
+export const TRANSLATION_CONFIG = {
+  defaultLanguage: 'en',
+  loader: {
+    provide: TranslateLoader,
+    useFactory: HttpLoaderFactory,
+    deps: [HttpClient],
+  },
+};
