b1713e2a758097a4be1c6c561021dc4255b75bb6
Vitalie <vitalie.cirjeleanu@gmail.com>
2024-05-17 11:42:23 +0300

    feat:(core) - Add translations for all the text content

--- src/app/components/overview/overview.component.ts
+++ src/app/components/overview/overview.component.ts
@@ -8,25 +8,42 @@ import {
 import { CommonModule } from '@angular/common';
 import { CardComponent } from '../card/card.component';
 import { Store } from '../../state';
-import { LoadStatus } from '../../../shared/types';
+import { LoadStatus, Topic } from '../../../shared/types';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'overview',
   standalone: true,
   templateUrl: './overview.component.html',
   styleUrl: './overview.component.scss',
-  imports: [CommonModule, CardComponent],
+  imports: [CommonModule, TranslateModule, CardComponent],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class OverviewComponent implements OnInit {
   private readonly store = inject(Store);
 
-  public readonly topics = this.store.topics;
+  public readonly topics = computed(() => this.getTopicsWithTranslationKeys());
   public readonly isLoading = computed(
     () => this.store.loadStatus() === LoadStatus.LOADING
   );
 
   public ngOnInit(): void {
     this.store.loadTopics();
+  }
+
+  private getTopicsWithTranslationKeys(): Topic[] {
+    return this.store.topics().map(
+      (topic): Topic => ({
+        ...topic,
+        name:
+          'topics.' +
+          topic.name
+            .split(' ')
+            .map((name, idx) =>
+              idx === 0 ? name : name[0].toUpperCase() + name.slice(1)
+            )
+            .join(''),
+      })
+    );
   }
 }
